488cab3c883820bcd0ea10521680b4295c0fe552
Marco Munizaga <git@marcopolo.io>
2025-10-29 09:54:34 -0700

    gofmt

--- netroute_stub.go
+++ netroute_stub.go
@@ -14,7 +14,7 @@ func New() (Router, error) {
 	rtr.ifaces[0] = net.Interface{}
 	rtr.addrs = make(map[int]ipAddrs)
 	rtr.addrs[0] = ipAddrs{}
-	rtr.v4 = []*rtInfo{&rtInfo{}}
-	rtr.v6 = []*rtInfo{&rtInfo{}}
+	rtr.v4 = []*rtInfo{{}}
+	rtr.v6 = []*rtInfo{{}}
 	return rtr, nil
 }
